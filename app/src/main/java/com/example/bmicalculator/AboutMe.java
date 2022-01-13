ead3f00564b2f9dc7db3806ff4677a0c90728ba3
fitrahh18 <fitrahbahsri01@gmail.com>
2022-01-13 23:50:58 +0800

    new1

--- app/src/main/java/com/example/bmicalculator/AboutMe.java
+++ app/src/main/java/com/example/bmicalculator/AboutMe.java
@@ -3,12 +3,22 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutMe extends AppCompatActivity {
+    TextView git, tube, mail;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
-
+        git = findViewById(R.id.mygithub);
+        git.setMovementMethod(LinkMovementMethod.getInstance());
+        tube = findViewById(R.id.myyt);
+        tube.setMovementMethod(LinkMovementMethod.getInstance());
+        mail = findViewById(R.id.myemail);
+        mail.setMovementMethod(LinkMovementMethod.getInstance());
     }
+
 }
